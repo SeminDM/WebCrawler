e8240264cc272fffbf8c36b6d4ec1bf3375f53b6
Dmitry Semin <amatoryslot@rambler.ru>
2021-01-02 17:23:49 +0300

    Add crawler types

---
+++ src/Crawler/CrawlerTypes.fs
@@ -0,0 +1,19 @@
+﻿module Crawler.CrawlerTypes
+
+open System
+
+type CrawlDocJob = { Uri: Uri }
+type CrawlJobWithImages = { Uri: Uri }
+
+type CrawlJob = 
+    | CrawlDocJob 
+    | CrawlJobWithImages
+
+type CrawlDocResult = { RootUri: Uri; Links: Uri list; ImageLinks: Uri list; Size: int }
+type CrawlImageResult = { ImageUri: Uri; Size: int }
+type CrawlFailedResult = { RootUri: Uri; Reason: string }
+
+type CrawlResult = 
+    | CrawlDocResult
+    | CrawlImageResult
+    | CrawlFailedResult
